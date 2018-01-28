e351da8fda18324ee0041911950c236c590bffd6
geordielad <rcottiss@tableau.com>
2018-01-27 21:54:17 -0500

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-4a0645221e9cac65954c.js","824195a0e802f5659b1ec0275d31c4ce"],["/app-f91c08f45acd18479683.js","e1781b694449e48bee2a9c4b838d9e06"],["/commons-ab5201dad5e6e7eaf4ee.js","0f708cfdc81d79f9c1f2c9795cb680ae"],["/index.html","0733edf89388fc2ea39d204b4503959a"],["/offline-plugin-app-shell-fallback/index.html","aa9588e8388364e81afbb429f9bc42ee"]];
+var precacheConfig = [["/app-4a0645221e9cac65954c.js","824195a0e802f5659b1ec0275d31c4ce"],["/app-684e03e5a4fe84f76145.js","d60e36a2775a9937dfe422e47d156956"],["/app-b96920350a809c28f17c.js","0d9320e6d429c23cd09b1943a73fe644"],["/app-f91c08f45acd18479683.js","e1781b694449e48bee2a9c4b838d9e06"],["/commons-ab5201dad5e6e7eaf4ee.js","89b3780e834cca014197e0dff929e6a7"],["/index.html","eac1c637fb01332951ac972ec3fccd0c"],["/offline-plugin-app-shell-fallback/index.html","0caa49e7e25658e56bceafbd517f3c78"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
